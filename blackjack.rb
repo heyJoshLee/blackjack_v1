7b5d7ef5137ba281fadee60a82b98237289a80f4
Josh Lee <Lee45276@gmail.com>
2015-06-14 21:36:36 +0800

    Added beginning deal

--- blackjack.rb
+++ blackjack.rb
@@ -1,9 +1,13 @@
-# Create an empty deck of cards
-deck = []
+require 'Pry'
 
-# Add cards
-begin
-  suits = ["spade", "club", "heart", "diamond"]
+
+player_hand = []
+dealer_hand = []
+
+# Add cards to deck
+def make_deck
+  new_deck = []
+  suits = ["spades", "clubs", "hearts", "diamonds"]
   faces_and_values = [ 
     {face: "A", value: 1}, {face: "2", value: 2}, {face: "3", value: 3},
     {face: "4", value: 4}, {face: "5", value: 5}, {face: "6", value: 6}, 
@@ -14,12 +18,68 @@ begin
 
   suits.each do |s|
     faces_and_values.each do |pair|
-      deck.push({suit: s, face: pair[:face], value: pair[:value]})
+      new_deck.push({suit: s, face: pair[:face], value: pair[:value]})
     end
   end
-p deck
+
+  new_deck.shuffle! # Returns a new shuffled deck
+end
+
+def draw_table(player_hand, dealer_hand)
+  system "clear"
+  puts "--------Dealer--------\n\n"
+  dealer_hand.each do |card|
+    print "#{card[:face]}\s\s"
+  end
+  puts "\n"
+
+  puts "--------Player--------"
+  player_hand.each do |card|
+    print "#{card[:face]}\s\s"
+  end
+  puts "\n"
 
 end
+
+# Creates a new shuffled deck
+deck = make_deck
+
+
+def deal_player_card(deck, player_hand)
+  if deck.length > 0
+    player_hand << deck.shift
+    new_card = player_hand.last
+    puts "Player draws a #{new_card[:face]} of #{new_card[:suit]}"
+  else
+    deck = make_deck
+    deal_player_card(player_hand)
+  end
+end
+
+def deal_dealer_card(deck, dealer_hand)
+  if deck.length > 0
+    dealer_hand << deck.shift
+    new_card = dealer_hand.last
+    puts "Dealer draws a #{new_card[:face]} of #{new_card[:suit]}"
+  end
+end
+
+def initial_deal(deck, player_hand, dealer_hand)
+  2.times do
+    draw_table(player_hand, dealer_hand)
+    deal_player_card(deck, player_hand)
+    draw_table(player_hand, dealer_hand)
+    deal_dealer_card(deck, dealer_hand)
+    draw_table(player_hand, dealer_hand)
+  end
+end
+
+initial_deal(deck, player_hand, dealer_hand)
+
+
+
+
+
 
 
 
